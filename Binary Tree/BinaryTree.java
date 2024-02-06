214542a160ccf9dc9b10073e9d3a86f4ff799746
imVetrivel <vetrivel2002.k@gmail.com>
2024-02-06 19:27:39 +0530

    msg

--- Binary Tree/BinaryTree.java
+++ Binary Tree/BinaryTree.java
@@ -1,17 +1,17 @@
 import java.util.*;
 
-
 public class BinaryTree {
     class Node {
         int data;
         Node left, right;
-    
+
         public Node(int n) {
             data = n;
-            left=null;
-            right=null;
+            left = null;
+            right = null;
         }
     }
-   Node root;
 
+    Node root;
+    
 }
--- Sorting/src/Quicksort.java
+++ Sorting/src/Quicksort.java
@@ -1,49 +1,43 @@
 import java.util.*;
-public class Quicksort
-{
-    public static void main(String[] args)
-    {
-        Scanner ab=new Scanner(System.in);
-        int n=ab.nextInt();
-        int a[]=new int[n];
-        for(int i=0;i<n;i++)
-        a[i]=ab.nextInt();
-        sort(a,0,a.length-1);
+
+public class Quicksort {
+    public static void main(String[] args) {
+        Scanner ab = new Scanner(System.in);
+        int n = ab.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = ab.nextInt();
+        sort(a, 0, a.length - 1);
+        System.out.println(Arrays.toString(a));
         ab.close();
     }
-    public static void sort(int a[],int l,int r)
-    {
-        if(l<r)
-        {
-            int mid = quick(a,l,r);
-            sort(a,l,mid-1);
-            sort(a,mid+1,r);
+
+    public static void sort(int a[], int l, int r) {
+        if (l < r) {
+            int pivotpos = quick(a, l, r);
+            sort(a, l, pivotpos - 1);
+            sort(a, pivotpos + 1, r);
         }
     }
-    public static int quick(int a[],int l,int r)
-    {
-        int start=l,end=r,pivot=a[l];
-        while(start<end)
-        {
-            while((start<=end)&&(a[start]<=pivot))
-            {
+
+    public static int quick(int a[], int l, int r) {
+        int start = l, end = r, pivot = a[l];
+        while (start < end) {
+            while ((start <= end) && (a[start] <= pivot)) {
                 start++;
             }
-            while((start<=end)&&(a[end]>pivot))
-            {
+            while ((start <= end) && (a[end] > pivot)) {
                 end--;
             }
-            if(start<end)
-            {
-                int temp=a[start];
-                a[start]=a[end];
-                a[end]=temp;
+            if (start < end) {
+                int temp = a[start];
+                a[start] = a[end];
+                a[end] = temp;
             }
         }
-        int temp=a[end];
-        a[end]=a[l];
-        a[l]=temp;
-        System.out.println(Arrays.toString(a));
+        int temp = a[end];
+        a[end] = a[l];
+        a[l] = temp;
         return end;
- }
+    }
 }
