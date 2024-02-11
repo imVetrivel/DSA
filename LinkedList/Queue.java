b2f416dcbae50f5aae726e6b222d787d4301c854
imVetrivel <vetrivel2002.k@gmail.com>
2024-02-11 20:27:58 +0530

    stack and queue added

---
+++ LinkedList/Queue.java
@@ -0,0 +1,57 @@
+package LinkedList;
+
+public class Queue {
+
+    public class Node {
+
+        int data;
+        Node next;
+
+        public Node(int n) {
+            data = n;
+            next = null;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+
+    // enqueue
+    public void enqueue(int n) {
+        Node newnode = new Node(n);
+        if (head == null) {
+            head = newnode;
+            tail = newnode;
+        } else {
+            tail.next = newnode;
+            tail = newnode;
+        }
+    }
+
+    // dequeue
+    public void dequeue() {
+        if (head == null) {
+            return;
+        }
+        head = head.next;
+    }
+
+    // display
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Queue q = new Queue();
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+        q.dequeue();
+        q.display();
+    }
+}
