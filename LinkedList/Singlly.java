6a9164e1722e1226c80e9c984a53e86516e4b870
imVetrivel <vetrivel2002.k@gmail.com>
2024-02-11 17:57:40 +0530

    message

--- LinkedList/Singlly.java
+++ LinkedList/Singlly.java
@@ -1,49 +1,110 @@
 package LinkedList;
-import java.util.*;
+
 public class Singlly {
 
     class Node {
         int data;
         Node next;
-        Node(int n)
-        {
-            data=n;
-            next=null;
+
+        Node(int n) {
+            data = n;
+            next = null;
         }
     }
-    public Node head=null;
-    public Node tail=null;
 
-    void add(int n)
-    {
-        Node temp=new Node(n);
-        if(head==null)
-        {
-            head=temp;
-            tail=temp;
+    public Node head = null;
+    public Node tail = null;
+
+    // insertion at first
+    public void addfirst(int n) {
+        Node temp = new Node(n);
+        if (head == null) {
+            head = temp;
+            tail = temp;
+        } else {
+            temp.next = head;
+            head = temp;
         }
-        else{
-            tail.next=temp;
-            tail=temp;
+    }
+
+    // insertion at middle
+    public void addmid(int n, int pos) {
+        Node newnode = new Node(n);
+        Node prev = head;
+        Node current = head;
+        int count = 0;
+        while (current != null && count <= pos - 1) {
+            prev = current;
+            current = current.next;
+            count++;
         }
+        prev.next = newnode;
+        newnode.next = current;
+    }
+
+    // insertion at last
+    void add(int n) {
+        Node temp = new Node(n);
+        if (head == null) {
+            head = temp;
+            tail = temp;
+        } else {
+            tail.next = temp;
+            tail = temp;
+        }
+    }
+
+    // deleteFirst Node
+    public void deletefirst() {
+        Node temp = head;
+        head = temp.next;
+        temp.next = null;
+    }
+
+    // deleteMiddle Node
+    public void deletemid(int pos) {
+        Node prev = head;
+        Node current = head;
+        int count = 0;
+        while (current != null && count <= pos - 1) {
+            prev = current;
+            current = current.next;
+            count++;
+        }
+        prev.next = current.next;
+        current.next = null;
+    }
+
+    // deleteLast Node
+    public void deletelast() {
+        Node temp = head;
+        while (temp.next != tail) {
+            temp = temp.next;
+        }
+        temp.next = null;
+        tail = temp;
     }
 
     // display
-    public void display(){
-        Node temp=head;
-        while(temp!=null)
-        {
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
             System.out.println(temp.data);
-            temp=temp.next;
+            temp = temp.next;
         }
     }
-    public static void main(String[] args){
-        // Scanner ab=new Scanner(System.in);
-        // int a=
-        Singlly s=new Singlly();
+
+    public static void main(String[] args) {
+
+        Singlly s = new Singlly();
         s.add(10);
         s.add(20);
         s.add(30);
+        s.add(50);
+        s.add(60);
+        s.addmid(40, 3);
+        s.display();
+        s.deletemid(3);
         s.display();
     }
 }
