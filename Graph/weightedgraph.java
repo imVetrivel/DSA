edff4692dec51299eaa2ea02d8b1c2ee05ba6c58
imVetrivel <vetrivel2002.k@gmail.com>
2024-03-12 11:24:17 +0530

    weighted graph

--- Graph/weightedgraph.java
+++ Graph/weightedgraph.java
@@ -4,112 +4,58 @@ import java.util.*;
 
 public class weightedgraph {
 
-    private Map<String, Node> nodes = new HashMap<>();
-    private Map<Node,List<Node>> adjacencylist = new HashMap<>();
-
-    public class Node {
-        private String label;
-
-        public Node(String label) {
-            this.label = label;
-        }
-
-        // @Override
-        public String toString() {
-            return label;
-        }                
-    }
-
-    public void addNode(String label) {
-        Node node = new Node(label);
-        nodes.putIfAbsent(label, node);
-        adjacencylist.putIfAbsent(node,new ArrayList<>());
-    }
-
-    public void addEdge(String from, String to) {
-        Node fromNode = nodes.get(from);
-        Node toNode = nodes.get(to);
-
-        if (fromNode == null) {
-            System.out.println("Node does not exists");
-            return;
-        }
-        if (toNode == null) {
-            System.out.println("Node does not exists");
-            return;
-        }
-        // directed graph
-        adjacencylist.get(fromNode).add(toNode);
-    }
-
-    public void print()
+   class Node
+   {
+    int n;
+    List<Edge> edges = new ArrayList<>();
+    Node(int n)
     {
-        for(Node source:adjacencylist.keySet())
-        {
-           if( !adjacencylist.get(source).isEmpty())          
-        System.out.println(source.label+" is connected to "+adjacencylist.get(source));
-            
-        }
+        this.n = n;
     }
 
-    public static void main(String[] args){
-    // Scanner ab = new Scanner(System.in);
-
-    weightedgraph w = new weightedgraph();
-    w.addNode("A");
-    w.addNode("B");
-    w.addNode("C");
-
-    w.addEdge("A", "C");
-    w.addEdge("A", "B");
-    w.addEdge("A", "A");
-    // w.print();
-    w.dfs("A");
-    }
-
-
-    // depthfirst search
-    public void dfs(String n)
+    public void addEdge(Node t,int weight)
     {
-        Node node = nodes.get(n);
-        if(node == null)
-        return;
-        dfs(node,new HashSet<>());
+      edges.add(new Edge(this,t,weight));
     }
 
-    public void dfs(Node n,Set<Node> node)
+    public List<Edge> getEdges()
     {
-        System.out.println(n);
-        node.add(n);
-
-        for(Node a: adjacencylist.get(n))
-        {
-            if(! node.contains(a))
-            {
-                dfs(a,node);
-            }
-        }
+      return edges;
     }
+   }
 
+   public Map<Integer,Node> nodes = new HashMap<>();
+   
+   class Edge
+   {
+    Node from;
+    Node to;
+    int weight;
+    Edge(Node from,Node to,int weight)
+    {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+   }
 
-    //  public void bfs(int v){
-    //     Queue<Integer> q = new LinkedList<>();
-    //     boolean visited[]=new boolean[n];
-    //     visited[v]=true;
-    //     q.add(v);
-    //     System.out.println(v);
-    //     while(!q.isEmpty())
-    //     {
-    //         v=q.poll();
-    //         for(int i=0;i<l[v].size();i++)
-    //         {
-    //             if(!visited[l[v].get(i)]){
-    //                 System.out.println(l[v].get(i));
-    //                 visited[l[v].get(i)]=true;
-    //                 q.add(l[v].get(i));
-    //             }
-    //         }
-    //     }
-    // }
+   public void addNode(int n)
+   {
+     nodes.putIfAbsent(n,new Node(n));
+   }
+
+   public void addEdge(int from, int to, int weight)
+   {
+    Node f = nodes.get(from);
+    Node t = nodes.get(to);
+
+    if(f == null)
+    return;
+    if(t == null)
+    return;
+
+    f.addEdge(t, weight);
+
+   }
 
 }
